4ab3f53cc013057d8610dda63be765f3d19aff9b
Usama Ali <usamaa18@gmail.com>
2022-05-15 19:51:28 -0600

    added "delete item" endpoint

--- routes/items.js
+++ routes/items.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { create } = require('../src/controllers/items/create');
 const { edit } = require('../src/controllers/items/edit');
+const { deleteItem } = require('../src/controllers/items/delete');
 
 router.post('/', (req, res) => {
   if ("itemId" in req.query) {
@@ -11,6 +12,11 @@ router.post('/', (req, res) => {
     console.log("creating");
     create(req, res);
   }
+});
+
+router.delete('/:itemId', (req, res) => {
+    console.log("deleting");
+    deleteItem(req, res);
 });
 
 
---
+++ src/controllers/items/delete.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Item = require("../../models/item");
+
+const deleteItem = async (req, res) => {
+    if (!req.params.itemId.match(/^[0-9a-fA-F]{24}$/)) {
+        res.status(400).send("Invalid itemId format, must follow MongoDB ObjectId requirements");
+        return;
+    }
+
+    let searchObj = { _id: mongoose.Types.ObjectId(req.query.itemId) };
+
+    Item.findOneAndDelete(
+        searchObj,
+        {},
+        (err, doc) => {
+            if (err) { res.status(400).send({ error: err }); }
+            else if (doc == null) {res.status(400).send("Invalid item (itemId not found in DB)");} 
+            else {
+                res.send(doc);
+            }
+        }
+    );
+
+}
+
+module.exports = { deleteItem };
